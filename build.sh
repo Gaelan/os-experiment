5a79edcd9e7f1a88cd7fa87e1add63ae3bf182b2
Tavi Kohn <XshadowlynxX@gmail.com>
2017-12-25 18:33:54 -0800

    Add quotes to properly handle unescaped characters (such as spaces) in paths from pwd

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "==> Buliding build container."
 CONTAINER_ID=$(docker build -q .)
 echo "==> Building."
-docker run --rm -v $(pwd):/project $CONTAINER_ID make
+docker run --rm -v "$(pwd)":/project $CONTAINER_ID make

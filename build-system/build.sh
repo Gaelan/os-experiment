93cc83a96ca791e6f1735eb4858d267959ac23bb
Tavi Kohn <XshadowlynxX@gmail.com>
2017-12-30 23:56:42 -0800

    Fix typo.

--- build-system/build.sh
+++ build-system/build.sh
@@ -1,4 +1,4 @@
-echo "==> Buliding build container."
+echo "==> Building build container."
 CONTAINER_ID=$(docker build -q .)
 echo "==> Building."
 docker run --rm -v "$(cd ../packages && pwd)":/project $CONTAINER_ID cd build-system && make

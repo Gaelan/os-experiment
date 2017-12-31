2b9c7f59e816455e69ee0ff3d6d1a8577183d627
Tavi Kohn <XshadowlynxX@gmail.com>
2017-12-31 00:09:59 -0800

    Update buildscript to correctly run make.

--- build-system/build.sh
+++ build-system/build.sh
@@ -2,4 +2,4 @@
 echo "==> Building build container."
 CONTAINER_ID=$(docker build -q .)
 echo "==> Building."
-docker run --rm -v "$(cd ../packages && pwd)":/project $CONTAINER_ID cd build-system && make
+docker run --rm -v "$(cd ../packages && pwd)":/project $CONTAINER_ID bash -c "cd build-system && make"

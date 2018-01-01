489f0ceeec7ffb5197ae4d4cad18318c3c82ce5a
Gaelan Steele <gbs@canishe.com>
2018-01-01 01:58:28 -0800

    Fix build system and add run step.

--- build-system/build.sh
+++ build-system/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
 echo "==> Building build container."
 CONTAINER_ID=$(docker build -q .)
 echo "==> Building."
-docker run --rm -v "$(cd ../packages && pwd)":/project $CONTAINER_ID bash -c "cd build-system && make"
+docker run --rm -v "$(cd .. && pwd)":/project $CONTAINER_ID bash -c "cd build-system && make"
+echo "==> Running in QEMU."
+qemu-system-x86_64 -cdrom experiment-x86_64.iso 

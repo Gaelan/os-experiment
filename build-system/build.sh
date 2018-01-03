b0ed1f8b5c2cd675d8d5bb9f00a0108fe2590d79
Tavi Kohn <XshadowlynxX@gmail.com>
2018-01-02 23:30:15 -0700

    Add commented debug QEMU invocation

--- build-system/build.sh
+++ build-system/build.sh
@@ -11,4 +11,5 @@ mkdir -p build/cache/cargo
 docker run -ti --rm -v "$(pwd)":/project -v "$(pwd)"/build/cache/xargo:/root/.xargo $CONTAINER_ID bash -c "cd build-system && make"
 echo "==> Running in QEMU."
 cd build-system
-qemu-system-x86_64 -cdrom ../build/experiment-x86_64.iso 
+qemu-system-x86_64 -cdrom ../build/experiment-x86_64.iso
+# qemu-system-x86_64 -d int --no-reboot -cdrom ../build/experiment-x86_64.iso

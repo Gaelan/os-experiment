fd7609f205bd24299df3b7ca9b0c8974de982a4c
Tavi Kohn <XshadowlynxX@gmail.com>
2018-03-21 18:10:24 -0600

    Allow build.sh to run without being in a tty.

--- build-system/build.sh
+++ build-system/build.sh
@@ -8,7 +8,7 @@ cd ..
 mkdir -p build
 mkdir -p build/cache
 mkdir -p build/cache/cargo
-docker run -ti --rm -v "$(pwd)":/project -v "$(pwd)"/build/cache/xargo:/root/.xargo $CONTAINER_ID bash -c "cd build-system && make"
+docker run -t --rm -v "$(pwd)":/project -v "$(pwd)"/build/cache/xargo:/root/.xargo $CONTAINER_ID bash -c "cd build-system && make"
 echo "==> Running in QEMU."
 cd build-system
 qemu-system-x86_64 -cdrom ../build/experiment-x86_64.iso
